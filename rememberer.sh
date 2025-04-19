339ee41d435580c5805e41d99253971a81568590
FelipeFMA <felipefmavelar@gmail.com>
2025-04-19 11:34:59 -0300

    added shbang

--- rememberer.sh
+++ rememberer.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env fish
+
+
 # Simple script to check if it's the first day of the month and remind me to download the new Arch ISO
 # I put that on my fish configuration so that it runs every time I open a new terminal
 # Just so that I remember to seed the new ISO :)
