022096abc8da49e29d0cc8582a7aa1ded13b38f7
FelipeFMA <felipefmavelar@gmail.com>
2025-05-13 15:21:14 -0300

    Refactor script to use dash instead of fish and update syntax for variable assignment and conditional statements.

--- rememberer.sh
+++ rememberer.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env fish
+#!/bin/dash
 
 
 # Simple script to check if it's the first day of the month and remind me to download the new Arch ISO
-# I put that on my fish configuration so that it runs every time I open a new terminal
+# I put that on my shell configuration so that it runs every time I open a new terminal
 # Just so that I remember to seed the new ISO :)
 
-set today (date +%d)
+today=$(date +%d)
 
-if test $today -eq 01
+if [ "$today" -eq 01 ]; then
     echo "Hoje é o primeiro dia do mês! Lembre-se de baixar a nova ISO do Arch Linux!"
-end
+fi
